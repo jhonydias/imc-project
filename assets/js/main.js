98d47198bf99d69f58ac2ff21fa9f44f58df559a
Jhony Dias <jhonymarlon@gmail.com>
2022-01-13 16:28:47 -0300

    imc-project-v1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     const inputPeso = e.target.querySelector('#peso');
     const inputAltura = e.target.querySelector('#altura');
+    console.log(inputPeso.value)
 
     const peso = Number(replaceStringPeso(inputPeso));
     const altura = Number(replaceStringAltura(inputAltura));
